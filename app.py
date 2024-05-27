43d6f0bfc6cd3133f835edd74d352d6b5d9a6c75
Da-Wei Lee <daviddwlee84@gmail.com>
2024-05-27 17:21:30 +0800

    Add OpenAI version finance title classification function

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from ollama import Client
 from llama_index.llms.ollama import Ollama
 import os
 from langchain_community.chat_models import ChatOllama
+from langchain_openai import ChatOpenAI
 from langchain_experimental.llms.ollama_functions import OllamaFunctions
 from langchain_core.pydantic_v1 import BaseModel, Field
 from langchain_core.output_parsers import JsonOutputParser, StrOutputParser
@@ -18,6 +19,7 @@ from fastapi import FastAPI
 app = FastAPI()
 
 BASE_URL: str = os.getenv("OLLAMA_BASE_URL", "http://192.168.222.236:11434")
+OPENAI_API_KEY: str = os.getenv("OPENAI_API_KEY")
 
 
 @app.get("/ollama")
@@ -218,7 +220,8 @@ def ollama_finance_title_classification(
         try:
             response = parse_and_check_json_markdown(
                 # raw_response, expected_keys=["answer", "justification"]
-                raw_response, expected_keys=["answer"]
+                raw_response,
+                expected_keys=["answer"],
             )
         except:
             response = raw_response
@@ -264,12 +267,86 @@ def ollama_finance_title_classification(
             # NOTE: basically only works in json mode
             response = parse_and_check_json_markdown(
                 # raw_response, expected_keys=["answer", "justification"]
-                raw_response, expected_keys=["answer"]
+                raw_response,
+                expected_keys=["answer"],
             )
         except:
             response = raw_response
     else:
         raise ValueError(f"Invalid mode: {mode}")
+    return response
+
+
+@app.get("/openai_finance_classification")
+def openai_finance_title_classification(
+    text: str,
+    model: Literal["gpt-3.5-turbo"] = "gpt-3.5-turbo",
+    timeout: int = 60,
+    api_key: str = OPENAI_API_KEY,
+    mode: Literal["v2_raw_json_parser"] = "v2_raw_json_parser",
+    force_json_mode: bool = True,
+):
+    if not mode.endswith("json_parser"):
+        raise NotImplementedError()
+    else:
+        llm = ChatOpenAI(
+            api_key=api_key,
+            model=model,
+            timeout=timeout,
+        )
+        if force_json_mode:
+            llm = llm.bind(response_format={"type": "json_object"})
+
+    if "v2_raw_json_parser":
+        format_instructions = """
+        回答需要符合以下JSON格式
+        ```json
+        {
+            "answer": "根据标题的内容，把数据打上标签，即对于该企业的态度：超预期、一般、低于预期、空值"
+            "justification": "详细条列判段过程，为什么给出此标签的理由"
+        }
+        ```
+
+        例如
+        title: 喜临门：“家具下乡”提振需求，股权激励赋能发展
+        json:
+        {
+            "answer": "超预期"
+            "justification": "1. 提振需求：标题中提到“家具下乡”政策提振需求，这是一个积极的市场信号，暗示企业可能会从中受益。 2. 股权激励赋能发展：股权激励通常被视为企业激励员工、推动企业长期发展的重要措施，暗示管理层对未来发展有信心。 3. 整体语气：整体语气积极，显然是在表达看好企业未来发展。"
+        }
+        title: 伟星新材系列深度报告之三：对标海天，伟星零售护城河优势尽显
+        json:
+        {
+            "answer": "一般"
+            "justification": "1. 系列深度报告：标题中提到这是系列深度报告的第三篇，表明这是对企业的全面分析的一部分，但并不直接表明态度。 2. 对标海天：提到对标另一家企业（海天），但未明确表达出看好或不看好的态度。 3. 优势尽显：虽然提到了“零售护城河优势尽显”，这是一个积极的描述，但没有使用非常强烈的表述来表明超预期的态度。"
+        }
+        title: 于模糊中寻找确定性——2022年食品饮料行业年度策略
+        json:
+        {
+            "answer": "空值",
+            "justification": "1. 行业年度策略：标题明确指出这是对整个食品饮料行业的年度策略分析，而不是针对某个具体企业的分析。 2. 于模糊中寻找确定性：这是一个较为笼统的描述，侧重于行业的整体策略和趋势，而非具体企业的表现或预期。 3. 缺乏具体企业信息：标题中没有提及任何具体企业，因此无法判断对某一企业的态度。"
+        }
+        """
+        # "justification": "1. 该标题并没有涉及具体企业的深度研究，而是对某个行业或行业的整体描述，无法反映对于特定企业的态度。 2. 标题中没有明确表达出看好或不看好的态度，也没有使用强烈的语言来表明超预期的态度。"
+        prompt = PromptTemplate(
+            template="现在有一批数据， 每一条数据都是分析师研报的标题，我希望根据标题的内容，把数据打上三种标签， 即对于该企业的态度（超预期 一般 低于预期）； 数据中存在以下几个问题： 第一条： 由于分析师普遍过度乐观或者不方便直接表述对于企业的看衰， 我希望在明显表述看好的标题才记作超预期，  隐含的描述不看好的即记作低于预期；  大部分的标题只是描述调研情况，无法反映看好或者不看好的态度；  第二条： 有的研报 是企业或者行业的综合性描述或一些例行公事的汇报， 不涉及具体企业的深度研究，这类研报不是某个企业的分析，标记为空值；\n{format_instructions}\ntitle: {title}\njson:\n",
+            input_variables=["title"],
+            partial_variables={"format_instructions": format_instructions},
+        )
+        chain = prompt | llm | StrOutputParser()
+        raw_response = chain.invoke({"title": text})
+        try:
+            # NOTE: basically only works in json mode
+            response = parse_and_check_json_markdown(
+                # raw_response, expected_keys=["answer", "justification"]
+                raw_response,
+                expected_keys=["answer"],
+            )
+        except:
+            response = raw_response
+    else:
+        raise ValueError(f"Invalid mode: {mode}")
+
     return response
 
 
